9b827ed7613fcb05da01b53c90b86487d44c5bc5
omaramen99 <omaramen741@gmail.com>
2022-12-03 19:07:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const app = require('./app');
+
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
+
